719d10f6dd2f792f7c493d2e66c02798fa159feb
Raitis Kriķis <raitis.krikis@gmail.com>
2016-12-10 15:37:15 +0200

    Removed debug println messages

--- app/controllers/LanguageController.scala
+++ app/controllers/LanguageController.scala
@@ -10,22 +10,18 @@ import play.api.i18n.{I18nSupport, Lang, MessagesApi}
 class LanguageController @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
   def changeLanguage() = Action { implicit request =>
-    println("Changing language")
     // Get URL of page the language was changed from, default to root
     val referer = request.headers.get(REFERER).getOrElse("/")
     // Define a single field form
     val form = Form("languageCode" -> nonEmptyText)
     // Populate the form with the data from the request
     form.bindFromRequest.fold(
-      erroneousForm => {
-        println("Language form had errors :(")
-        Redirect(referer)
-      },
+      erroneousForm => Redirect(referer),
       languageCode => Redirect(referer).withLang(languageCode match {
-        case "lv" => println("Changing language to latvian"); Lang("lv")
-        case "en" => println("Changing language to english"); Lang("en")
-        case "ru" => println("Changing language to russian"); Lang("ru")
-        case _ => println("Unknown language code, changing to default"); Lang(Lang.defaultLang.code)
+        case "lv" => Lang("lv")
+        case "en" => Lang("en")
+        case "ru" => Lang("ru")
+        case _ => Lang("lv")
       })
     )
   }
