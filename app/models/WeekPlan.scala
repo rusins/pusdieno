b9d075367606eb92e595cb6648bd816bc8833143
Raitis Kriķis <raitis.krikis@gmail.com>
2016-12-23 19:49:53 +0200

    Improved time tables

--- app/models/WeekPlan.scala
+++ app/models/WeekPlan.scala
@@ -8,15 +8,25 @@ object WeekPlan {
   val dayToInt = Map("Monday" -> 0, "Tuesday" -> 1, "Wednesday" -> 2,
     "Thursday" -> 3, "Friday" -> 4, "Saturday" -> 5, "Sunday" -> 7)
 
-  def printTime(time: (Int, Int)): String = {
+  def open(time: (Int, Int)) = time._2 != 0
+
+  def printTime(time: (Int, Int))(implicit messages: Messages): String = if (!open(time))
+    messages("eateries.closed")
+  else {
     val res = StringBuilder.newBuilder
     res.append((time._1 / 100).toString)
     res.append(":")
-    res.append(time._1 % 100)
+    time._1 % 100 match {
+      case 0 => res.append("00")
+      case x => res.append(x)
+    }
     res.append(" – ")
     res.append(time._2 / 100)
     res.append(":")
-    res.append(time._2 % 100)
+    time._2 % 100 match {
+      case 0 => res.append("00")
+      case x => res.append(x)
+    }
     res.toString()
   }
 }
@@ -26,21 +36,24 @@ class WeekPlan(data: Array[(Int, Int)]) {
 
   lazy val uniqueCount = data.map((x: (Int, Int)) => (x, 1)).toMap.size
 
-  def display(today: Int, maxLines: Int = 2)(implicit messages: Messages, lang: Lang) = Html(
+  def open(today: Int) = WeekPlan.open(data(today))
+
+  private def short(x: Int)(implicit messages: Messages): String = x match {
+    case 0 => messages("date.short.monday")
+    case 1 => messages("date.short.tuesday")
+    case 2 => messages("date.short.wednesday")
+    case 3 => messages("date.short.thursday")
+    case 4 => messages("date.short.friday")
+    case 5 => messages("date.short.saturday")
+    case 6 => messages("date.short.sunday")
+  }
+
+  def displayInRows(today: Int, maxLines: Int = 2)(implicit messages: Messages) = Html(
     if (uniqueCount > maxLines)
-      Messages("date.today") + "<br>" + print(data(today))
+      messages("date.today") + "<br>" + WeekPlan.printTime(data(today))
     else {
       val used = Array.fill(7)(false)
       val res = StringBuilder.newBuilder
-      def short(x: Int): String = x match {
-        case 0 => Messages("date.short.monday")
-        case 1 => Messages("date.short.tuesday")
-        case 2 => Messages("date.short.wednesday")
-        case 3 => Messages("date.short.thursday")
-        case 4 => Messages("date.short.friday")
-        case 5 => Messages("date.short.saturday")
-        case 6 => Messages("date.short.sunday")
-      }
       for (i <- data.indices) {
         if (!used(i)) {
           for (j <- 0 until i)
@@ -64,4 +77,20 @@ class WeekPlan(data: Array[(Int, Int)]) {
     }
 
   )
+
+  def displayInTable(tableClasses: String = "")(implicit messages: Messages) = Html({
+
+    val res = StringBuilder.newBuilder
+    res.append("<table class='")
+    res.append(tableClasses)
+    res.append("'>\n")
+    val used = Array.fill(7)(false)
+    for (i <- data.indices) {
+      res.append("<tr>\n<td>")
+
+      res.append("</td>\n</tr>")
+    }
+    res.append("</table>")
+    res.toString()
+  })
 }
