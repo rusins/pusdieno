8a3dfbddfc276749f4956337b52077a174af5e1d
Raitis Kriķis <raitis.krikis@gmail.com>
2017-01-03 07:34:09 +0200

    Activator deleted all of my previous commits, so just redoing stuff like the database table mapping for User and managing the time classes in the models.

--- app/models/User.scala
+++ app/models/User.scala
@@ -1,16 +1,48 @@
 package models
 
-object User {
-  val ziedonis = User("Imants Ziedonis", Some("ziedonis.jpg"), Some(22578303), Some("11.30 - 12.30"), fav = true, hungry = false, cafe = true, Nil)
-  val dalai = User("Dalai Lama", Some("dalai.jpg"), None, Some("11.45 - 12.15"), fav = true, hungry = false, cafe = true, Nil)
-  val steve = User("Steve Buscemi", Some("steve.jpg"), Some(77654873), Some("14.00 - 16.00"), fav = true, hungry = true, cafe = false, Nil)
-  val margaret = User("Margaret Thatcher", Some("margaret.jpg"), Some(65892397), Some("11.00 - 12.30"), fav = true, hungry = true, cafe = true, Nil)
-  val tyrion = User("Tyrion Lannister", Some("tyrion.jpg"), None, None, fav = false, hungry = true, cafe = false, Nil)
-  val vaira = User("Vaira Vīķe Freiberga", Some("vaira.jpg"), Some(22884993), Some("11.30 - 12.30"), fav = false, hungry = false, cafe = true, Nil)
-  val martins = User("Mārtiņš Rītiņš", Some("martins.jpg"), None, None, fav = false, hungry = true, cafe = true, Nil)
-  val twisty = User("Twisty the clown", Some("twisty.jpg"), Some(911), Some("24/7"), fav = false, hungry = false, cafe = false, Nil)
-  val pawly = User("Pawly No-Nose", None, None, None, fav = true, hungry = false, cafe = true, Nil)
-  val users = List(ziedonis, dalai, steve, margaret, tyrion, vaira, martins, twisty, pawly)
-}
+import java.sql.Time
+import java.util.UUID
 
-case class User(name: String, image: Option[String], phone: Option[Int], schedule: Option[String], fav: Boolean, hungry: Boolean, cafe: Boolean, location: List[String])
+import slick.driver.PostgresDriver.api._
+
+case class User(id: UUID = UUID.randomUUID(), name: String, phoneNumber: Option[Int], eatsAt: WeekPlan)
+
+class UserTable(tag: Tag) extends Table[User](tag, "USER") {
+  def id: Rep[UUID] = column[UUID]("ID", O.PrimaryKey)
+
+  def name: Rep[String] = column[String]("NAME")
+
+  def phoneNumber: Rep[Option[Int]] = column[Option[Int]]("PHONE")
+
+  def monday: Rep[Option[Time]] = column[Option[Time]]("MONDAY")
+
+  def tuesday: Rep[Option[Time]] = column[Option[Time]]("TUESDAY")
+
+  def wednesday: Rep[Option[Time]] = column[Option[Time]]("WEDNESDAY")
+
+  def thursday: Rep[Option[Time]] = column[Option[Time]]("THURSDAY")
+
+  def friday: Rep[Option[Time]] = column[Option[Time]]("FRIDAY")
+
+  def saturday: Rep[Option[Time]] = column[Option[Time]]("SATURDAY")
+
+  def sunday: Rep[Option[Time]] = column[Option[Time]]("SUNDAY")
+
+  private type WeekTupleType =
+    (Option[Time], Option[Time], Option[Time], Option[Time], Option[Time], Option[Time], Option[Time])
+  private type UserTupleType = (UUID, String, Option[Int], WeekTupleType)
+
+  private val userShapedValue = (id, name, phoneNumber, (
+    monday, tuesday, wednesday, thursday, friday, saturday, sunday
+  )).shaped[UserTupleType]
+
+  private val toModel: UserTupleType => User = {
+    case (id, name, phoneNumber, eatsAt) => User(id, name, phoneNumber, WeekPlan.tupled(eatsAt))
+  }
+
+  private val toTuple: User => Option[UserTupleType] =
+    user => Some((user.id, user.name, user.phoneNumber, WeekPlan.unapply(week)))
+
+  def * = userShapedValue <> (toModel, toTuple)
+
+}
