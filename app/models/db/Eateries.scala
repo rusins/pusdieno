74ac65a406bbac41548af8d0b15561877964776c
Raitis Kriķis <raitis.krikis@gmail.com>
2017-01-09 22:07:46 +0200

    COMPILES!!! Saving progress just in case

--- app/models/db/Eateries.scala
+++ app/models/db/Eateries.scala
@@ -5,10 +5,11 @@ import java.util.UUID
 
 import models.OpenTimes
 import slick.driver.PostgresDriver.api._
+import slick.lifted.ShapedValue
 
-case class Eatery(id: UUID = UUID.randomUUID, chain: String, streetAddress: String, openTimes: OpenTimes)
+case class Eatery(id: UUID = UUID.randomUUID, chain: String, streetAddress: String, openTimes: Option[OpenTimes])
 
-class EateryTable(tag: Tag) extends Table(tag, "eateries") {
+class EateryTable(tag: Tag) extends Table[Eatery](tag, "eateries") {
 
   def id: Rep[UUID] = column[UUID]("id", O.PrimaryKey)
 
@@ -44,27 +45,49 @@ class EateryTable(tag: Tag) extends Table(tag, "eateries") {
 
   def sundayClose: Rep[Option[Time]] = column[Option[Time]]("sunday_close")
 
-  private type OpenTimesTupleType = Option[(Time, Time, Time, Time, Time, Time, Time, Time, Time, Time, Time, Time, Time, Time)]
+  private type OpenTimesTupleType = (Option[Time], Option[Time], Option[Time], Option[Time], Option[Time], Option[Time],
+    Option[Time], Option[Time], Option[Time], Option[Time], Option[Time], Option[Time], Option[Time], Option[Time])
   private type EateryTupleType = (UUID, String, String, OpenTimesTupleType)
 
-  // TODO: Learn cats and convert tuple of options to an option of a tuple
-
-  private val EateryShapedValue: EateryTupleType = (id, chain, streetAddress, (mondayOpen, mondayClose, tuesdayOpen, tuesdayClose,
-    wednesdayOpen, wednesdayClose, thursdayOpen, thursdayClose, fridayOpen, fridayClose,
-    thursdayOpen, thursdayClose, fridayOpen, fridayClose, saturdayOpen, saturdayClose,
-    sundayOpen, sundayClose)).shaped[EateryTupleType]
+  private val eateryShapedValue = (id, chain, streetAddress, (
+    mondayOpen, mondayClose,
+    tuesdayOpen, tuesdayClose,
+    wednesdayOpen, wednesdayClose,
+    thursdayOpen, thursdayClose,
+    fridayOpen, fridayClose,
+    saturdayOpen, saturdayClose,
+    sundayOpen, sundayClose
+  )
+  ).shaped[EateryTupleType]
 
   private def toModel: EateryTupleType => Eatery = {
-    case (id, chain, address, (a, b, c, d, e, f, g)) => Eatery(id, chain, address, OpenTimes(a, b, c, d, e, f, g))
+    case (id, chain, address, (a, b, c, d, e, f, g, h, i, j, k, l, m, n)) =>
+      Eatery(id, chain, address, for {
+        mo <- a
+        mc <- b
+        tuo <- c
+        tuc <- d
+        wo <- e
+        wc <- f
+        tho <- g
+        thc <- h
+        fo <- i
+        fc <- j
+        sao <- k
+        sac <- l
+        suo <- m
+        suc <- n
+      } yield OpenTimes((mo, mc), (tuo, tuc), (wo, wc), (tho, thc), (fo, fc), (sao, sac), (suo, suc)))
   }
 
-  private def toTuple: Eatery => Option[EateryTupleType] = eatery => Some {
-    def o = eatery.openTimes
+  private def toTuple: Eatery => Option[EateryTupleType] =
+    eatery => Some(eatery.id, eatery.chain, eatery.streetAddress, eatery.openTimes match {
+      case Some(OpenTimes((a, b), (c, d), (e, f), (g, h), (i, j), (k, l), (m, n))) =>
+        (Some(a), Some(b), Some(c), Some(d), Some(e), Some(f), Some(g), Some(h), Some(i), Some(j), Some(k), Some(l), Some(m), Some(n))
+      case None => (None, None, None, None, None, None, None, None, None, None, None, None, None, None)
+    }
+    )
 
-    (eatery.id, eatery.chain, eatery.streetAddress, ((o.monday._1, o.monday._2), (o.tuesday._1, o.tuesday._2),
-      (o.wednesday._1, o.wednesday._2), (o.thursday._1, o.thursday._2), (o.friday._1, o.friday._2),
-      (o.saturday._1, o.saturday._2), (o.sunday._1, o.sunday._2)))
-  }
-
-  def * = <> (toModel, toTuple)
+  def * = eateryShapedValue <> (toModel, toTuple)
 }
+
